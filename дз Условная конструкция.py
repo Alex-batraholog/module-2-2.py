2fd02429bab4c8627d4da22877dee7ad8b946b38
Alex-batraholog <alexvasvas78@gmail.com>
2024-07-20 15:53:24 +0300

    Add files via upload

---
+++ дз Условная конструкция.py
@@ -0,0 +1,21 @@
+first=int(input('Ведите число 1: '))
+second=int(input('Ведите число 2: '))
+third=int(input('Ведите число 3: '))
+if first==second==third:
+    print(3)
+elif first==second or first==third or second==third:
+    print(2)
+if first!=second and first!=third and second!=third:
+    print(0)
+
+
+
+
+
+
+
+
+
+
+
+
